1eba59be7a5abce117f5d1b0e68b08d3a211f6e3
Sadik Shaikh <sadikmshaikh34@gmail.com>
2023-10-19 19:10:26 +0530

    Update completion.py

--- app/completion.py
+++ app/completion.py
@@ -2,7 +2,7 @@
 import os
 import openai
 from dotenv import load_dotenv
-from prompt import completion_prompt
+from prompt import completion_prompt, MESSGAE
 
 
 load_dotenv()
@@ -22,3 +22,20 @@ def completion_api_predict(comment) -> str:
     presence_penalty=0
   )
   return response["choices"][0]["text"]
+
+def chat_completion_model(comment) -> str:
+  prompt = {
+        "role": "user",
+        "content": comment
+      }
+  MESSGAE.append(prompt)
+  response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages= MESSGAE,
+    temperature=0,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+  )
+  return response["choices"][0]["message"]["content"]
